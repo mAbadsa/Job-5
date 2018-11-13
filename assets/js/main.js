eec56604bce1cd6514e2327c2942a8da618c214c
mAbadsa <moh22389@gmail.com>
2018-11-13 16:48:44 +0200

    Update assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
 $(document).ready(function($) { 
-//      $( 'ul.navbar-nav li' ).on( 'click', function() {
-//            $( this ).parent().find( 'li.active' ).removeClass( 'active' );
-//            $( this ).addClass( 'active' );
-//      });
-    
     $('ul.navbar-nav a').filter(function(){return           this.href==location.href}).addClass('active').siblings().removeClass('active');
         $('ul.navbar-nav a').click(function(){
             $(this).siblings().removeClass('active');
