9c01edc27320775dc6e10e3a5cb38804dc0cbfb9
mAbadsa <moh22389@gmail.com>
2018-11-13 16:37:28 +0200

    active class change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function($) { 
-        $( 'ul.navbar-nav li' ).on( 'click', function() {
-            $( this ).parent().find( 'li.active' ).removeClass( 'active' );
-            $( this ).addClass( 'active' );
-      });
+//      $( 'ul.navbar-nav li' ).on( 'click', function() {
+//            $( this ).parent().find( 'li.active' ).removeClass( 'active' );
+//            $( this ).addClass( 'active' );
+//      });
     
+            $('ul.navbar-nav a').filter(function(){return           this.href==location.href}).addClass('active').siblings().removeClass('active')
+                $('ul.navbar-nav a').click(function(){
+                $(this).addClass('active').siblings().removeClass('active')	
+            })
 });
