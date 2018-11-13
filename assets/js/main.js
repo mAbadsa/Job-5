22065ccfdc1e4759f161c7c587c2a4c9cbb4e635
mAbadsa <moh22389@gmail.com>
2018-11-13 16:43:24 +0200

    active class

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,9 @@ $(document).ready(function($) {
 //            $( this ).addClass( 'active' );
 //      });
     
-            $('ul.navbar-nav a').filter(function(){return           this.href==location.href}).addClass('active').siblings().removeClass('active')
-                $('ul.navbar-nav a').click(function(){
-                $(this).addClass('active').siblings().removeClass('active')	
-            })
+    $('ul.navbar-nav a').filter(function(){return           this.href==location.href}).addClass('active').siblings().removeClass('active');
+        $('ul.navbar-nav a').click(function(){
+            $(this).siblings().removeClass('active');
+            $(this).addClass('active').siblings().removeClass('active');
+    })
 });
