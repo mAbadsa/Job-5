4a0b4e1c09ccaa4c31bef49dae0b2ba1495ef404
mAbadsa <moh22389@gmail.com>
2018-11-13 16:01:03 +0200

    more changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,7 @@
-var owl = $('.upSlide');
-owl.owlCarousel({
-//    autoplay:true,
-//    autoplayTimeout:1000,
-    autoplayHoverPause:true,
-    loop:true,
-    margin:1,
-    nav:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false,
-        },
-        600:{
-            items:1,
-            nav:false,
-        },
-        1000:{
-            items:1
-        },
-        1200:{
-            items:1
-        }
-    }
+$(document).ready(function($) { 
+        $( 'ul.navbar-nav li' ).on( 'click', function() {
+            $( this ).parent().find( 'li.active' ).removeClass( 'active' );
+            $( this ).addClass( 'active' );
+      });
+    
 });
